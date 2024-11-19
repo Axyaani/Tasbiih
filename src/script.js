93250d01d446af995582f7d38d23683a9e6a9030
axyaani <axyaani@gmail.com>
2024-11-18 21:55:00 -0930

    tasbiih

---
+++ src/script.js
@@ -0,0 +1,10 @@
+let result=document.querySelector('.result');
+function tasbiih(){
+  result.textContent=parseInt(result.textContent)+1;
+  return
+}
+function reset(){
+  result.textContent=0;
+}
+
+
